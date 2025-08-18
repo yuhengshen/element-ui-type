2a154378de26bf78c6a44979e00a2a224dd9257f
yuhengshen <yuhengshen@qq.com>
2025-08-18 09:56:57 +0800

    chore: 更新 package.json 版本至 1.0.14，移除 rate 组件的多余注释和 main.vue 文件

--- packages/card/index.vue
+++ packages/card/index.vue
@@ -6,11 +6,6 @@
 <script setup lang="ts">
 import { CSSProperties } from 'vue';
 
-/*
- * header	设置 header，也可以通过 slot#header 传入 DOM	string	—	—
- * body-style	设置 body 的样式	object	—	{ padding: '20px' }
- * shadow	设置阴影显示时机	string	always / hover / never	always 
- */
 defineProps<{
   /**
    * 设置 header，也可以通过 slot#header 传入 DOM
