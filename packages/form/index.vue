a9e51af7295f228dad3ea02a514772a2f449edd4
yuhengshen <yuhengshen@qq.com>
2025-09-03 11:14:43 +0800

    fix: 更新 package.json 版本至 1.0.15，调整表单组件的 props 类型为可选

--- packages/form/index.vue
+++ packages/form/index.vue
@@ -13,47 +13,47 @@ defineProps<{
   /**
    * 行内表单模式
    */
-  inline: boolean;
+  inline?: boolean;
   /**
    * 表单域标签的位置，如果值为 left 或者 right 时，则需要设置 label-width
    */
-  labelPosition: "right" | "left" | "top";
+  labelPosition?: "right" | "left" | "top";
   /**
    * 表单域标签的宽度，例如 '50px'。作为 Form 直接子元素的 form-item 会继承该值。支持 auto。
    */
-  labelWidth: string;
+  labelWidth?: string;
   /**
    * 表单域标签的后缀
    */
-  labelSuffix: string;
+  labelSuffix?: string;
   /**
    * 是否隐藏必填字段的标签旁边的红色星号
    */
-  hideRequiredAsterisk: boolean;
+  hideRequiredAsterisk?: boolean;
   /**
    * 是否显示校验错误信息
    */
-  showMessage: boolean;
+  showMessage?: boolean;
   /**
    * 是否以行内形式展示校验信息
    */
-  inlineMessage: boolean;
+  inlineMessage?: boolean;
   /**
    * 是否在输入框中显示校验结果反馈图标
    */
-  statusIcon: boolean;
+  statusIcon?: boolean;
   /**
    * 是否在 rules 属性改变后立即触发一次验证
    */
-  validateOnRuleChange: boolean;
+  validateOnRuleChange?: boolean;
   /**
    * 用于控制该表单内组件的尺寸
    */
-  size: "medium" | "small" | "mini";
+  size?: "medium" | "small" | "mini";
   /**
    * 是否禁用该表单内的所有组件。若设置为 true，则表单内组件上的 disabled 属性不再生效
    */
-  disabled: boolean;
+  disabled?: boolean;
 }>();
 
 defineEmits<{
--- types/form/index.vue.d.ts
+++ types/form/index.vue.d.ts
@@ -11,47 +11,47 @@ type __VLS_Props = {
     /**
      * 行内表单模式
      */
-    inline: boolean;
+    inline?: boolean;
     /**
      * 表单域标签的位置，如果值为 left 或者 right 时，则需要设置 label-width
      */
-    labelPosition: "right" | "left" | "top";
+    labelPosition?: "right" | "left" | "top";
     /**
      * 表单域标签的宽度，例如 '50px'。作为 Form 直接子元素的 form-item 会继承该值。支持 auto。
      */
-    labelWidth: string;
+    labelWidth?: string;
     /**
      * 表单域标签的后缀
      */
-    labelSuffix: string;
+    labelSuffix?: string;
     /**
      * 是否隐藏必填字段的标签旁边的红色星号
      */
-    hideRequiredAsterisk: boolean;
+    hideRequiredAsterisk?: boolean;
     /**
      * 是否显示校验错误信息
      */
-    showMessage: boolean;
+    showMessage?: boolean;
     /**
      * 是否以行内形式展示校验信息
      */
-    inlineMessage: boolean;
+    inlineMessage?: boolean;
     /**
      * 是否在输入框中显示校验结果反馈图标
      */
-    statusIcon: boolean;
+    statusIcon?: boolean;
     /**
      * 是否在 rules 属性改变后立即触发一次验证
      */
-    validateOnRuleChange: boolean;
+    validateOnRuleChange?: boolean;
     /**
      * 用于控制该表单内组件的尺寸
      */
-    size: "medium" | "small" | "mini";
+    size?: "medium" | "small" | "mini";
     /**
      * 是否禁用该表单内的所有组件。若设置为 true，则表单内组件上的 disabled 属性不再生效
      */
-    disabled: boolean;
+    disabled?: boolean;
 };
 /**
  * 对整个表单进行校验的方法，参数为一个回调函数。该回调函数会在校验结束后被调用，并传入两个参数：是否校验成功和未通过校验的字段。
